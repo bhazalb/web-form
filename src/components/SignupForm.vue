279c819c5af3a46f06df5fd7604141f16ca0ce59
bhazalb <bayramhazal030@gmail.com>
2023-11-07 19:49:53 +0300

    finish

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,11 +1,12 @@
 
 <template>
-   <form >
+   <form @submit.prevent="handleSubmit" >
     <label>Email:</label>
     <input type="email" required  v-model="email">
 
     <label>Password:</label>
     <input type="password" required  v-model="password">
+    <div v-if="passwordError" class="error" > {{ passwordError }} </div>
 
     <label >Role: </label>
     <select v-model="role">
@@ -15,15 +16,17 @@
 
     <label>Skills: </label>
     <input type="text" v-model="tempSkill" @keyup="addSkill" >
-    <div v-for="skill in skills"  :key="skill">,
-      
+    <div v-for="skill in skills"  :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
    <div class="terms">
     <input type="checkbox" required v-model="terms">
     <label>Acceot terms and conditions</label>
    </div>
-
+   <div class="submit">
+    <button>Create an Account</button>
+   </div>
    
 
    </form>
@@ -33,7 +36,7 @@
   <p>Password: {{ password }} </p>
   <p>Role : {{ role }} </p>
   <p> Terms Accepted:  {{ terms }} </p>
-  <p>Skill: {{ tempSkill }} </p>
+ 
 
 </template>
 <script>
@@ -44,15 +47,39 @@
             password: '',
             role : '',
             terms: false , 
+            tempSkill: '',
             skills: [],
+            passwordError: '',
         }
     },
    methods: {
     addSkill(e){
         if(e.key === ',' && this.tempSkill){
-            this.skills.push(this.tempSkill)
+            if(!this.skills.includes(this.tempSkill)) {
+                this.skills.push(this.tempSkill)
+            }
+            
             this.tempSkill =''
         }
+    },
+    deleteSkill(skill){
+    this.skills = this.skills.filter((item) => {
+        return skill !== item 
+    })
+    },
+    handleSubmit() {
+      //validate password
+      this.passwordError =this.password.length > 5 ?
+       '' : 'password must be at least 6 chars long '
+ 
+       if(!this.passwordError){
+        console.log('email:' , this.email)
+        console.log('password:' , this.password)
+        console.log('role:' , this.role)
+        console.log('terms accepted:' , this.terms)
+
+       }
+
     }
    }
      
@@ -93,6 +120,38 @@ input[type="checkbox"]{
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+.pill{
+    display: inline-block;
+    margin: 28px 10px 0 0 ;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+button{
+    background: rgb(0, 42, 105);
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+button:hover{
+    background: rgb(88, 126, 184);
+}
+.submit{
+    text-align: center;
+}
+.error{
+    color: #ec1010;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 
 </style>
